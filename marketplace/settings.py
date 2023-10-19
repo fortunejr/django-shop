5812f465a6283f0e90554f1b6b4472d82d65e3aa
FortuneJr <70198021+fortunejr@users.noreply.github.com>
2023-10-19 07:06:28 +0100

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Marketplace',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
